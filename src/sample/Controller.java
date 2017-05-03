5202af9aa1fa5ab0afd99ddec9f28ca42fc1d252
Meirinf <meirin.f@gmail.com>
2017-05-03 20:38:53 +0200

    He empezado a Realizar el metodo de Ultimos Datos recogidos

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    // Campos que actualizaremos en cada sección
+
+    public TextField campoTexto1;
+    public TextField campoTexto2;
+    public TextField campoTexto3;
+    public TextField campoTexto4;
+    public TextField campoTexto5;
+    public TextField campoTexto6;
+
+    public void UltimosDatos(ActionEvent actionEvent){
+
+        campoTexto1.setPromptText("Titulo");
+        campoTexto2.setPromptText("Autor ");
+        campoTexto3.setPromptText("Editorial");
+        campoTexto4.setPromptText("Numero de paginas");
+        campoTexto5.setPromptText("Año de edicion ");
+        campoTexto6.setPromptText("Numero de ejemplares");
+
+
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,9 +12,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("MeteoPoblenou");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        Scene scene = new Scene(new Group(), 500, 400);
+        scene.getStylesheets().add("src/style.css");
     }
 
 
