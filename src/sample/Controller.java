8e77c06fc2cdbd63dad136689d2c464811489bd8
Meirinf <meirin.f@gmail.com>
2017-05-04 16:54:39 +0200

    Muestra los Textos al elegir la opción del menu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,29 +1,40 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class Controller {
 
     // Campos que actualizaremos en cada sección
 
-    public TextField campoTexto1;
-    public TextField campoTexto2;
-    public TextField campoTexto3;
-    public TextField campoTexto4;
-    public TextField campoTexto5;
-    public TextField campoTexto6;
+    public Text campoTexto1;
+    public Text campoTexto2;
+    public Text campoTexto3;
+    public Text campoTexto4;
+    public Text campoTexto5;
 
+
+    //Lo que hara al inicarse
+    public void initialize() {
+
+    }
+
+    //Meotodo que mostrara los ultimos datos recogidos
+    
     public void UltimosDatos(ActionEvent actionEvent){
 
-        campoTexto1.setPromptText("Titulo");
-        campoTexto2.setPromptText("Autor ");
-        campoTexto3.setPromptText("Editorial");
-        campoTexto4.setPromptText("Numero de paginas");
-        campoTexto5.setPromptText("Año de edicion ");
-        campoTexto6.setPromptText("Numero de ejemplares");
+        campoTexto1.setText("Temperatura Raspberry: ");
+        campoTexto2.setText("Humedad: ");
+        campoTexto3.setText("Presión: ");
+        campoTexto4.setText("Tiempo: ");
+        campoTexto5.setText("Temperatura Api: ");
 
-
+        //Muetro los textos
+        campoTexto1.setVisible(true);
+        campoTexto2.setVisible(true);
+        campoTexto3.setVisible(true);
+        campoTexto4.setVisible(true);
+        campoTexto5.setVisible(true);
     }
 
 }
